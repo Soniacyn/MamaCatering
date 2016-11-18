6c2fcc56bc94a5b5adb867ad7dfbb844259b3cc2
Astrif <fadilahastri21@gmail.com>
2016-11-18 15:03:45 +0700

    Register Activity

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl506162636/mamacatering/RegisterActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl506162636/mamacatering/RegisterActivity.java
@@ -9,5 +9,19 @@ import android.widget.TextView;
  * Created by Astri Fadilah on 18/11/2016.
  */
 public class RegisterActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
 
+        TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
+        loginScreen.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View arg0)
+            {
+                finish();
+            }
+        });
+    }
 }
