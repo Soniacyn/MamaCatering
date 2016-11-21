ee524ec39a1a54882e6b1cc2ce4fdb29375b829b
Sonia Cynthia Elchi <soniacynthia05@gmail.com>
2016-11-21 09:48:16 +0700

    foto.

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl506162636/mamacatering/activity/LoginActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl506162636/mamacatering/activity/LoginActivity.java
@@ -21,6 +21,7 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+import id.sch.smktelkom_mlg.project.xiirpl506162636.mamacatering.MainActivity;
 import info.androidhive.loginandregistration.R;
 import info.androidhive.loginandregistration.app.AppConfig;
 import info.androidhive.loginandregistration.app.AppController;
